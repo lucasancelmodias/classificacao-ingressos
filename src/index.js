94fcca19de8b82f884d921f8ca87f66bd21df149
Lucas Ancelmo Dias <lucas.ancelmo547@gmail.com>
2021-04-28 21:56:13 -0300

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const axios = require("axios")
+const app = express();
+app.use(express.json());
+
+const palavraChave = "importante";
+
+const funcoes = {
+    ClienteCriado: (cliente) => {
+        cliente.status = cliente.idade >= 60 ?
+            "importante" :
+            "comum";
+        axios.post('http://localhost:10000/eventos', {
+            tipo: "ClienteClassificada",
+            dados: cliente,
+        });
+    },
+};
+
+app.post('/eventos', (req, res) => {
+    try {
+        funcoes[req.body.tipo](req.body.dados);
+    } catch (err) {}
+    res.status(200).send({ msg: "ok" });
+});
+
+app.listen(7000, () => console.log("Classificação. Porta 7000"));
